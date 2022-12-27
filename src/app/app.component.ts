519bc2ead0e579ab773558d8a459f385ef08220d
Yuri An <yuriandev@gmail.com>
2022-12-27 20:31:21 +0600

    init: first commit. First steps with event binding, property binding and interpolation syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pw';
+  includeLetters = false;
+  password = '';
+
+  onChangeUserLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick() {
+    console.log(this.includeLetters);
+    this.password = 'My password';
+  }
 }
