b8f34ce1975cf8d098f9262f85509c48de86589d
Yuri An <yuriandev@gmail.com>
2022-12-28 23:43:46 +0600

    feat: add tracking additional properties, handling input and generating password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pw';
+  length = 0;
   includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
   onChangeUserLetters() {
     this.includeLetters = !this.includeLetters;
   }
 
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: Event) {
+    const parsedValue = parseInt((<HTMLInputElement>value.target).value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
   onButtonClick() {
-    console.log(this.includeLetters);
-    this.password = 'My password';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
